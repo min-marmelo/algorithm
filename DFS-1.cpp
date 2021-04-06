5049eb93891ffe794265b6b63a4b8aca2e4e5d53
DESKTOP-B0Q2J6N\민교 <alsry4547@gmail.com>
2021-04-06 20:54:10 +0900

    DFS 경로탐색

---
+++ DFS-1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n,m, cnt = 0;
+vector<vector<int> > v(21,vector<int>(21,0));
+int ch[21];	// 이거 중요. 체크 배열!! 
+void DFS(int vertex) 
+{
+	int i;
+	if(vertex == n) // 내가 원하는 지점
+	{
+		++cnt;
+	}
+	else
+	{
+		for(i = 1; i <= n; ++i)
+		{
+			if(v[vertex][i] == 1 && ch[i] == 0) // 갈수있는지, 그리고 방문을 안했는지.
+			{
+				ch[i] = 1; // 방문해줬다고 체크.
+				DFS(i);
+				ch[i] = 0;	 
+			} 
+		}
+	}
+}
+
+int main(void)
+{	
+	cin >> n >> m;
+	int i, a,b;
+	for (i = 0; i < m; ++i)
+	{
+		cin >> a >> b;
+		v[a][b] = 1;
+	}
+	ch[1] = 1;
+	DFS(1);
+	cout << cnt;
+	// 세갈래 +,-,참여X 
+
+
+}
